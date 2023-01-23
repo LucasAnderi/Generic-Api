ed1aa96851c1d56465ab20e12546251d30dda19c
lucasanderi <luc.anderi22@gmail.com>
2023-01-23 14:20:18 -0300

    feat: findById, update and deleteById

--- generic-project/generic-database/src/main/java/org/example/db/dao/impl/GameDaoImpl.java
+++ generic-project/generic-database/src/main/java/org/example/db/dao/impl/GameDaoImpl.java
@@ -6,10 +6,7 @@ import org.example.model.entities.Game;
 import org.example.model.enums.GameType;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,7 +37,7 @@ public class GameDaoImpl implements GameDao<Game> {
                 game.setId(resultSet.getInt("id"));
                 String gameType = resultSet.getString("genero");
                 game.setType(GameType.valueOf(gameType));
-                game.setName(resultSet.getString("nome");
+                game.setName(resultSet.getString("nome"));
                 game.setDescription(resultSet.getString("descricao"));
 
             }
@@ -58,7 +55,42 @@ public class GameDaoImpl implements GameDao<Game> {
 
     @Override
     public Game findById(int id) {
-        return null;
+        Game item = null;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet= null;
+
+        final String sql ="SELECT * FROM jogo WHERE id= ?;";
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1,id);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                item = new Game();
+                item.setId(resultSet.getInt("id"));
+                item.setName(resultSet.getString("nome"));
+                item.setDescription(resultSet.getString("descricao"));
+                item.setPrice(resultSet.getDouble("preco"));
+                item.setDevelopedBy(resultSet.getString("desenvolvido_por"));
+                item.setReleaseDate(resultSet.getString("data_lancamento"));
+
+                String gameType = resultSet.getString("tipo");
+                item.setType(GameType.valueOf(gameType));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            ConnectionFactory.close(preparedStatement,connection,resultSet);
+        }
+
+        return item;
     }
 
     @Override
@@ -68,12 +100,79 @@ public class GameDaoImpl implements GameDao<Game> {
 
     @Override
     public boolean update(Game entity) {
-        return false;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+
+        String sql ="UPDATE jogo SET nome = ?, ";
+        sql += "ultima_modificacao = ?, descricao = ?";
+        sql += "WHERE";
+        sql += "id = ?;";
+
+        try {
+            connection = ConnectionFactory.getConnection();
+
+            connection.setAutoCommit(false);
+
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, entity.getName());
+
+            preparedStatement.setTimestamp(2,new Timestamp(System.currentTimeMillis()));
+
+            preparedStatement.setString(3,entity.getDescription());
+
+            preparedStatement.execute();
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            return false;
+        }finally{
+            ConnectionFactory.close(preparedStatement,connection);
+        }
     }
 
     @Override
     public boolean deleteById(int id) {
-        return false;
+
+        boolean result = false;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        final String sql = "DELETE FROM jogo WHERE id = ?;";
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+
+            connection.setAutoCommit(false);
+
+            preparedStatement.execute();
+            connection.commit();
+            result = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } finally {
+            ConnectionFactory.close(preparedStatement, connection);
+        }
+
+        return result;
+
     }
 
 
